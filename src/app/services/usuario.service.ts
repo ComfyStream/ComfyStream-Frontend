77d1f8c70852e6137f0bc69e6f052d01972beba5
serperrui1 <serperrui1@alum.us.es>
2021-03-19 13:11:11 +0100

    login y logout con inicio de pruebas

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { tap} from 'rxjs/operators';
 import { Router } from '@angular/router';
-const base_url = environment.base_url;
+const base_url = environment.apiUrl;
 
 @Injectable({
   providedIn: 'root'
