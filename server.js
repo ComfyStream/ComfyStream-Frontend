dead3085977c8d21c23ffba900889206c94d8961
serperrui1 <serperrui1@alum.us.es>
2021-04-03 01:35:02 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/Comfy-Stream-frontend'));
-app.get('*', function(req, res) {
-    res.sendFile(`./Comfy-Stream-frontend/dist/index.html`);
+app.use(express.static(__dirname + '/dist/comfy-stream-frontend'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/comfy-stream-frontend/index.html'));
 });
 app.listen(process.env.PORT || 8080);
