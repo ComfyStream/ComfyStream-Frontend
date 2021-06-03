252941e6b6b3a38101e04695d7fedf33233b8a70
serperrui1 <serperrui1@alum.us.es>
2021-06-03 19:38:49 +0200

    arreglos de feedback

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'https://comfystream-backend-ppl.herokuapp.com/comfystream/api',
+  apiUrl:'https://comfystream-final.herokuapp.com/comfystream/api',
   paypalClienteId : "Aabt74GDM3PGqbRhGctct4k6Rra-dHBpG4NMlgQlckJOcwBYqdr1jS_QJt3yLWI3niGc1vQlui-BK1tm"
 };
 
